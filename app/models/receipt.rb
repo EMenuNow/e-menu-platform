3673f67bd082f838ea6f6a83f5524d77717bcec1
James Grant <1993jamesgrant@gmail.com>
2021-04-11 11:11:00 +0100

    print specific

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -146,7 +146,7 @@ class Receipt < ApplicationRecord
     print_receipt = ""
     group = self.find_grouped_receipts.reverse
     group.each do |x|
-      print_receipt += ApplicationController.render(partial: "manager/live/order_item_screen_specific_print", locals: {grouped: true, screen_item: self, restaurant: restaurant_id, item_screen_type_key: item_screen_type_key })
+      print_receipt += ApplicationController.render(partial: "manager/live/order_item_screen_specific_print", locals: {grouped: true, receipt: x, restaurant: restaurant_id, item_screen_type_key: item_screen_type_key })
     end.empty? and begin
       return
     end
