c74496a0c13e938e5f3a5368dbd44d65e72c6f9a
Simon Lee <6236517+level7webdev@users.noreply.github.com>
2021-07-04 16:46:01 +0100

    task to clone between restaurants

--- app/models/custom_list.rb
+++ app/models/custom_list.rb
@@ -7,16 +7,21 @@ class CustomList < ApplicationRecord
 
 
   def input_type
-  
   	if constraint == '1'
   		'radio'
   	else
   		'checkbox'
   	end
+  end
 
+  def clone!(cl_id, restaurant_id)
+    custom_list = CustomList.find(cl_id)
+    new_restaurant = Restaurant.find(restaurant_id)
 
-
-
+    new_custom_list = custom_list.dup
+    new_custom_list.restaurant_id = new_restaurant.id
+    new_custom_list.save
+    new_custom_list
   end
 
 end
--- app/models/custom_list_item.rb
+++ app/models/custom_list_item.rb
@@ -7,4 +7,15 @@ class CustomListItem < ApplicationRecord
 
   validates_presence_of :price, :on => [:create, :update], :message => "please include a price or 0 for no price"
   delegate :name, to: :custom_list, prefix: true
+
+  def clone!(cli_id, cl_id)
+    custom_list_item = CustomListItem.find(cli_id)
+    new_custom_list = CustomList.find(cl_id)
+
+    new_custom_list_item = custom_list_item.dup
+    new_custom_list_item.custom_list_id = new_custom_list.id
+    new_custom_list_item.save
+    custom_list_item.categorisations_clis.each{|c| c.clone!(c.id, new_custom_list_item.id)}
+    new_custom_list_item
+  end
 end
